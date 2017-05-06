039c13a9c286c7b62295f5b5ab78b67a03da622c
Joy Wan <wanzhiyu@outlook.com>
2017-05-06 10:27:42 +0800

    Site updated: 2017-05-06 10:27:39

--- js/src/mobile.js
+++
@@ -1,9 +0,0 @@
-if (document.documentElement && document.documentElement.clientHeight && document.documentElement.clientWidth) {             
-    var winHeight = document.documentElement.clientHeight;
-    var winWidth = document.documentElement.clientWidth;
-    if (winWidth < 400) {
-        var el = document.getElementById('main');
-        el.style.backgroundImage = 'none';
-        el.style.backgroundColor = 'rgba(240,240,240,.7)';
-    }
-}
